5538b0bc18c1b5667747eac94ad0c10d3fea9f25
Bulty-T <shraddhaforpython@proton.me>
2024-09-15 09:17:23 +0000

    array

---
+++ scripts.js
@@ -0,0 +1,85 @@
+//const name = "hi"
+//console.log("hi");
+// string : concatenation , template literal
+
+//const age = 31;
+//const user ="elon"; 
+
+//("hello elon, your age is 31. nice to meet you");
+
+//function do something (){
+    //console.log("hi");
+  //  return 21;
+//}
+
+//ES6 string templates literals, created using backstring ie ``
+// interpolate (insert) [ie ${} ]: expression ,value 
+
+//const finalstr = "hi" + user + "your age is"
+
+//const userName = prompt("enter your name");
+//const finalstr = `hi ${userName}, nice to meet you.`;
+//console.log(finalstr);
+
+//Ternary (3) operator, Expression
+//const a = true ? "1" : "0";
+//console.log(a);
+
+//user age
+//  >18 ? major : minor
+
+//const teststr = "hello ECMAscript";
+//let x ;
+//x = teststr;
+//console.log(x[teststr.length -1]);
+
+//js: collections
+//array & objects
+
+//const a =[
+    //[1,2,3]
+  //  [4,5,6]
+//];
+//console.log(a[1][1]);
+
+//const str = "apple - banana - orange - mango - guava";
+//console.log(str.split("_"));
+
+// create a function that reverses a string
+// reverse("hello")=>"olleh"
+
+//function reverse(string){
+ //const inputArr = string.split("");
+ //inputArr.reverse();
+ //const reversestr = inputArr.join("");
+ //return reversestr;
+//}
+//
+//const output = reverse("cricket");
+//console.log(output);
+
+//function reverseNum(Number){
+  //  const inputArr = Number.split("");
+    //inputArr.reverse();
+    //const reversestr = inputArr.join("");
+    //return reversestr;
+//}
+//
+//const output = reverseNum("21");
+//console.log(output);
+
+//const fruit = ("banana,guava,mango,dragon_fruit").split(",");
+//console.log(fruit);
+
+//for ( i=0; i<fruit.length; i++);
+//{
+  //  console.log(i);
+//}
+
+//const a = ["a","b","c","d"];
+//console.log(a);
+// slice, splice: insertion deletion
+//
+//a.splice (1, 0, "hi", "game");
+//console.log(a);
+
